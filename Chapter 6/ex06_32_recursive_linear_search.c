d95a4d036565c121287600573f7a328912da0644
arthurskb <accjxd@gmail.com>
2025-11-24 20:12:00 -0300

    adding ex06_32

---
+++ Chapter 6/ex06_32_recursive_linear_search.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int linearSearch(int array[], int length, int key);
+
+int main() {
+	int arraySize;
+	printf("%s ", "Insert the size of the array:");
+	scanf("%d", &arraySize);
+	int array[arraySize];
+	for (int i = 0; i < arraySize; i++) {
+		printf("%s%d%s", "Insert the ", i+1, "º element: ");
+		scanf("%d", &array[i]);
+	}
+	int key;
+	printf("%s ", "Insert the key number you want to find:");
+	scanf("%d", &key);
+	int index = linearSearch(array, arraySize-1, key);
+	if (index == -1) printf("%s%d%s\n", "Sorry, the number ", key, " wasn't found.");
+	else printf("%s%d%s%d%s\n", "The number ", key, " was found on the ", index, "º position!"); 
+}
+
+int linearSearch(int array[], int length, int key) {
+	if (length < 0) return -1;
+	else if (array[length] == key) return length+1;
+	else return linearSearch(array, length-1, key);
+}
