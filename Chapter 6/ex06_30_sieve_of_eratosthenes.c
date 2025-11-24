69b3a4f1af334fe3b5027a178dc0ef265cda2c35
arthurskb <accjxd@gmail.com>
2025-11-24 14:35:34 -0300

    adding ex06_30

---
+++ Chapter 6/ex06_30_sieve_of_eratosthenes.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define SIZE 1000 
+
+int main() {
+	int array[SIZE];	
+	for (size_t i = 0; i < SIZE; i++) {
+		array[i] = 1;
+	}
+	for (size_t i = 2; i < SIZE; i++) {
+		if (array[i] == 1) {
+			for (size_t j = 2; i*j < SIZE; j++) {
+				array[i*j] = 0;
+				//printf("%ld\n", i*j);
+			}
+		}
+	}
+	for (size_t i = 2; i < SIZE; i++) {
+		if(array[i] == 1) printf("%ld%s\n", i, " is a prime number.");
+	}
+	return 0;
+}
