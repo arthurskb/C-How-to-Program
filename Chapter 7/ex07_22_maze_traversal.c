571a4c310772a61a115e9dc38e4b59dfc037b9e8
arthurskb <accjxd@gmail.com>
2025-12-19 23:37:54 -0300

    adds ex07_22 and ex07_23

---
+++ Chapter 7/ex07_22_maze_traversal.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+#define DIMENSIONS 12
+
+typedef enum {NORTH, EAST, SOUTH, WEST} Direction;
+
+int mazeTraverse(char maze[][DIMENSIONS], const int rowStartingLocation, const int columnStartingLocation);
+void printMaze(char (*maze)[DIMENSIONS]);
+
+int main() {
+  int rowStartingLocation = 2;
+  int columnStartingLocation = 0;
+  char maze[DIMENSIONS][DIMENSIONS] = {
+        "############",
+        "#...#......#",
+        "..#.#.####.#",
+        "###.#....#.#",
+        "#....###.#..",
+        "####.#.#.#.#",
+        "#..#.#.#.#.#",
+        "##.#.#.#.#.#",
+        "#........#.#",
+        "######.###.#",
+        "#......#...#",
+        "############"
+    };
+  int result = mazeTraverse(maze, rowStartingLocation, columnStartingLocation);
+  if (result == 1) printf("An exit was found!\n");
+  else printf("There's no exit\n");
+  return 0;
+}
+
+int mazeTraverse(char maze[][DIMENSIONS], int currentX, int currentY) {
+  int checkMove(char maze[][DIMENSIONS], int newX, int newY);
+  static int initialized = 0;
+  static Direction currentFace;
+  static int initialX = -1;
+  static int initialY = -1;
+  static int moveX[] = {-1, 0, 1, 0};
+  static int moveY[] = {0, 1, 0, -1};
+
+  maze[currentX][currentY] = 'X';
+  printf("initialX = %d e initialY = %d\n", initialX, initialY);
+  printf("currentX = %d e currentY = %d\n", currentX, currentY);
+  printMaze(maze);
+
+  if (!initialized) {
+    currentFace = EAST;
+    initialX = currentX;
+    initialY = currentY;
+    initialized = 1;
+  }
+  else if ((currentX == 0 || currentX == 11) || (currentY == 0 || currentY == 11)) {
+    return 1;
+  }
+  else if ((currentX == initialX) && (currentY == initialY)) {
+    return 0;
+  }
+
+  for (size_t i = 0; i < 4; i++) {
+    int dir = (currentFace+i)%4;
+    int newX = currentX + moveX[dir];
+    int newY = currentY + moveY[dir];
+
+    if (checkMove(maze, newX, newY)) {
+      currentFace = (dir + 3) % 4;
+      return mazeTraverse(maze, newX, newY);
+    }
+  }
+
+
+}
+
+int checkMove(char maze[][DIMENSIONS], int newX, int newY) {
+  if (newX < 0 || newX >= 12 || newY < 0 || newY >= 12) return 0;
+  char check = maze[newX][newY];
+  if (check == '#') return 0;
+  if (check == '.' || check == 'X') return 1;
+}
+
+void printMaze(char (*maze)[DIMENSIONS]) {
+  for (size_t i = 0; i < DIMENSIONS; i++) {
+    for (size_t j = 0; j < DIMENSIONS; j++) {
+      printf("%c", maze[i][j]);
+    }
+    printf("\n");
+  }
+  printf("------------\n");
+}
---
+++ Chapter 7/ex07_23_maze_generator.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define DIMENSIONS 12
+typedef enum {NORTH, EAST, SOUTH, WEST} Direction;
+
+int mazeGenerator(char maze[][DIMENSIONS], int startingPoint[], int endingPoint[]);
+void printMaze(char (*maze)[DIMENSIONS]);
+
+int main() {
+  int startingPoint[2] = {-1, -1};
+  int endingPoint[2] = {-1, -1};
+  char maze[DIMENSIONS][DIMENSIONS];
+  srand(time(NULL));
+  mazeGenerator(maze, startingPoint, endingPoint);
+  printMaze(maze);
+  printf("StartingPoint = {%d, %d}\n", startingPoint[0], startingPoint[1]);
+  printf("EndingPoint = {%d, %d}\n", endingPoint[0], endingPoint[1]);
+}
+
+
+void printMaze(char (*maze)[DIMENSIONS]) {
+  for (size_t i = 0; i < DIMENSIONS; i++) {
+    for (size_t j = 0; j < DIMENSIONS; j++) {
+      printf("%c", maze[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
+
+int mazeGenerator(char maze[][DIMENSIONS], int startingPoint[], int endingPoint[]) {
+  void initializeMaze(char (*maze)[DIMENSIONS]);
+  int checkMove(int newX, int newY);
+  void shuffleDir(Direction dirs[]);
+
+  static int initialized = 0;
+
+  static int currentX = 1;
+  static int currentY = 1;
+  maze[1][1] = '.';
+
+  if (!initialized) {
+    initializeMaze(maze);
+    initialized = 1;
+  }
+
+  printMaze(maze);
+
+  Direction dirs[] = {NORTH, EAST, SOUTH, WEST};
+  shuffleDir(dirs);
+
+  for (size_t i = 0; i < 4; i++) {
+    int moveX = 0;
+    int moveY = 0;
+    if (dirs[i] == NORTH) moveX = -2;
+    else if (dirs[i] == EAST) moveY = 2;
+    else if (dirs[i] == SOUTH) moveX = 2;
+    else if (dirs[i] == WEST) moveY = -2;
+    int nextX = currentX + moveX;
+    int nextY = currentY + moveY;
+    int midX = currentX + moveX/2;
+    int midY = currentY + moveY/2;
+    if (checkMove(nextX, nextY) && maze[nextX][nextY] == '#') {
+      maze[nextX][nextY] = '.';
+      maze[midX][midY] = '.';
+      currentX += moveX;
+      currentY += moveY;
+      return mazeGenerator(maze, startingPoint, endingPoint);
+      currentX -= moveX;
+      currentY -= moveY;
+    }
+  }
+  int entranceRow = 0;
+  for (size_t i = 1; i < 11; i++) {
+    if (maze[i][1] == '.') {
+      entranceRow = i;
+    }
+  }
+  maze[entranceRow][0] = '.';
+  startingPoint[0] = entranceRow;
+  startingPoint[1] = 0;
+  int exitRow = -1;
+  for (size_t i = 1; i < 11; i++) {
+    if (maze[i][9] == '.') {
+      exitRow = i;
+    }
+  }
+  if (exitRow > 0) {
+    maze[exitRow][10] = '.';
+    maze[exitRow][11] = '.';
+    endingPoint[0] = exitRow;
+    endingPoint[1] = 11;
+  }
+  return 1;
+}
+
+void shuffleDir(Direction dirs[]) {
+  int auxRand;
+  int auxDirs[4] = {-1, -1, -1, -1};
+  for (size_t i = 0; i < 4; i++) {
+    while(1) {
+      auxRand = rand() % 4;
+      if (auxDirs[auxRand] == -1) {
+        auxDirs[auxRand] = i;
+        break;
+      }
+    }
+  }
+  for (size_t i = 0; i < 4; i++) {
+    dirs[i] = auxDirs[i];
+  }
+}
+
+int checkMove(int newX, int newY) {
+  if (newX < 1 || newX >= DIMENSIONS-1 || newY < 1 || newY >= DIMENSIONS-1) return 0;
+  return 1;
+}
+
+void initializeMaze(char (*maze)[DIMENSIONS]) {
+  for (size_t i = 0; i < DIMENSIONS; i++) {
+    for (size_t j = 0; j < DIMENSIONS; j++) {
+      maze[i][j] = '#';
+    }
+  }
+}
