b43efa78741db36c5a920eed33df2989f12d5fa5
arthurskb <accjxd@gmail.com>
2026-01-13 10:48:48 -0300

    adds ex08_09

---
+++ Chapter 8/ex08_09_concatenating_strings.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLENGTH 100
+
+void readString(char str[]);
+void printStringInfo(char str[]);
+
+int main() {
+  char s1[MAXLENGTH];
+  char s2[MAXLENGTH];
+  readString(s1);
+  readString(s2);
+  printStringInfo(s1);
+  printStringInfo(s2);
+  strcat(s1, s2);
+  printStringInfo(s1);
+}
+
+void readString(char str[]) {
+  printf("%s",
+         "Insert a one-line string to be read: (Max size = 49 characters)\n");
+  fgets(str, 50, stdin);
+  int num = strlen(str);
+  str[num - 1] = '\0';
+}
+
+void printStringInfo(char str[]) {
+  printf("%s%s%s%lu%s", "The string is:\n", str, "\nWith a length of ",
+         strlen(str), " characters.\n");
+}
